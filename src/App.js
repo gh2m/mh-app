4cc60be3ae88223ef1dd690be1f7f428a591f5ba
gh2m <gh2m@yahoo.com>
2023-12-19 22:06:48 -0600

    created form with 4 fields

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import "./styles.css";
 import { useState, useEffect } from "react";
 
 export default function App() {
-
+  return (<form>
+    <div>
+      <label for="fname">First name:</label>
+      <input type="text" id="fname" name="fname" />
+    </div>
+    <div>
+      <label for="lname">Last name:</label>
+      <input type="text" id="lname" name="lname" />
+    </div>
+    <div>
+      <label for="email">Email:</label>
+      <input type="text" id="email" name="email" />
+    </div>
+    <div>
+      <label for="phonenum">Phone Number:</label>
+       <input type="text" id="phonenum" name="phonenum" />
+    </div>
+  </form>)
 }
